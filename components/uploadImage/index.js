030e58d6e12fe7ebc051af693c7dd65f125aeb04
pmnhatit <pmn.99.20@gmail.com>
2021-03-06 20:52:41 +0700

    fix upload image

--- components/uploadImage/index.js
+++ components/uploadImage/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const passPort = require('../../services/passport');
 const uploadImageController = require('./uploadImageController');
 
-router.put('/:user_id', uploadImageController.uploadImage);
+router.put('/avatar/:user_id', uploadImageController.uploadImage);
 
 module.exports = router;
