d6cb5365db4327e8e11dc13e646839097ca20abd
pmnhatit <pmn.99.20@gmail.com>
2021-03-06 20:49:44 +0700

    fix upload image

--- components/uploadImage/index.js
+++ components/uploadImage/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const passPort = require('../../services/passport');
 const uploadImageController = require('./uploadImageController');
 
-router.post('/:user_id', uploadImageController.uploadImage);
+router.put('/:user_id', uploadImageController.uploadImage);
 
 module.exports = router;
