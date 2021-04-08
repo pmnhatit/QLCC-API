60b3e7b39131a21712f4330358f7f72031139b51
pmnhatit <pmn.99.20@gmail.com>
2021-04-08 21:29:08 +0700

    fix other-bill apis

--- components/otherBill/index.js
+++ components/otherBill/index.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const passPort = require('../../services/passport');
 const otherBillController = require('./otherBillController');
 
+router.get('/:bill_id', passPort.authenticate('jwt',{session: false}), otherBillController.getOtherBillById);
+
 router.get('/all/:apart_id', passPort.authenticate('jwt',{session: false}), otherBillController.getBillByApartmentId);
 
 router.get('/month-bill/:apart_id/:month/:year', passPort.authenticate('jwt',{session: false}), otherBillController.getBillByMonth);
-
-router.post('/add', passPort.authenticate('jwt',{session: false}), otherBillController.createOtherBill);
 
 module.exports = router;
--- components/otherBill/otherBillController.js
+++ components/otherBill/otherBillController.js
@@ -3,34 +3,30 @@ const otherBillServices = require('./otherBillServices');
 module.exports.getBillByApartmentId = async (req, res, next)=>{
     try {
         const apart_id = req.params.apart_id;
-        console.log("user_id: ", apart_id);
         const all_bill = await otherBillServices.getOtherBillByApartmentId(apart_id);
         res.json({data: all_bill});
     } catch (error) {
         console.log("errors: ",error);
-        res.status(500);
+        res.status(500).json(error);
     }
 }
 module.exports.getBillByMonth = async (req, res, next) =>{
     try {
         const {apart_id, month, year} = req.params;
-        console.log("month: ",month);
         const month_bill = await otherBillServices.getOtherBillByMonth(apart_id, month, year);
         res.json({data: month_bill});
     } catch (error) {
         console.log("errors: ",error);
-        res.status(500);
+        res.status(500).json(error);
     }
 }
-module.exports.createOtherBill = async (req, res, next) =>{
+module.exports.getOtherBillById = async (req, res, next) =>{
     try {
-        const {apart_id, apart_management, n_motobikes, n_cars, maintenance_fee, 
-            service_charge, other_fees, month, year, note} = req.body;
-        const new_bill = await otherBillServices.createOtherBill(apart_id, apart_management, n_motobikes, n_cars, maintenance_fee, 
-            service_charge, other_fees, month, year, note);
-        res.json({data: new_bill});
+        const {bill_id} = req.params;
+        const bill = await otherBillServices.getOtherBillById(bill_id);
+        res.status(200).json({data: bill});
     } catch (error) {
-        console.log("errors: ", error);
-        res.status(500);
+        console.log("errors: ",error);
+        res.status(500).json(error);
     }
 }
